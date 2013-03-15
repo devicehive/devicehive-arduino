d9e214da04fb4f3fe090390ac1a5a258544c65ac
Sergey Polichnoy <pilatuz@gmail.com>
2013-03-15 17:25:46 +0400

    Artyom's updates/changes

--- DeviceHive/examples/Example1/Example1.ino
+++ DeviceHive/examples/Example1/Example1.ino
@@ -5,24 +5,41 @@
 #define CMD_RESULT_OK           "OK"
 
 // LedCube device registration data
+// Intent numbers should be greater than 255
+// Please refer to URL for complete syntax of registration info 
 const char *REG_DATA = "{"
-    "id:\"f4b07b4d-5b02-47e7-bd9b-c8d56e0cfdd1\","
-    "key:\"Arduino_LED\","
-    "name:\"Arduino LED\","
-    "deviceClass:{"
-        "name:\"Arduino_LED\","
-        "version:\"1.0\"},"
-    "equipment:[{code:\"led\",name:\"led\",type:\"led\"}],"
-    "commands:["
-        "{intent:256,name:\"set\",params:u8}"
+    "\"id\":\"f4b07b4d-5b02-47e7-bd9b-c8d56e0cfdd1\","
+    "\"key\":\"Arduino_LED\","
+    "\"name\":\"Arduino LED\","
+    "\"deviceClass\":{"
+        "\"name\":\"Arduino_LED\","
+        "\"version\":\"1.0\"},"
+    "\"equipment\":[{\"code\":\"led\",\"name\":\"led\",\"type\":\"led\"}],"
+    "\"commands\":["
+        "{\"intent\":1000,\"name\":\"set\",\"params\":\"u8\"},"
+        "{\"intent\":1001,\"name\":\"blink\",\"params\":{\"on\":\"u16\",\"off\":\"u16\",\"count\":\"u8\"}}"
     "],"
-    "notifications:["
-        "{intent:257,name:\"button\",params:u8}"
+    "\"notifications\":["
+        "{\"intent\":2000,\"name\":\"button\",\"params\":\"u8\"}"
     "]"
 "}";
 
 InputMessage rx_msg; // received message
 const int LED_PIN = 13;
+const int BUTTON_PIN = 12;
+
+int oldButtonState;
+
+// VERY IMPORTANT: the order and types of fields in struct should be exactly the same as those
+// defined in initialization string *REG_DATA
+// {\"intent\":257,\"name\":\"blink\",\"params\":{\"on\":\"u16\",\"off\":\"u16\",\"count\":\"u8\"}}
+
+struct BlinkParam
+{
+  short on;
+  short off;
+  byte count;  
+};
 
 void setLedState(int state)
 {
@@ -31,8 +48,8 @@ void setLedState(int state)
 
 void sendButtonState(int state)
 {
-    OutputMessage tx_msg(257);
-    tx_msg.putUInt8(state);
+    OutputMessage tx_msg(2000);
+    tx_msg.putByte(state);
     DH.write(tx_msg);
 }
 
@@ -42,6 +59,11 @@ void sendButtonState(int state)
 void setup(void)
 {
     Serial.begin(115200);
+    
+    pinMode(LED_PIN, OUTPUT);
+    pinMode(BUTTON_PIN, INPUT_PULLUP); // ... so you don't need a pull-up resistor
+    
+    oldButtonState = digitalRead(BUTTON_PIN);
 
     DH.begin(Serial);
     DH.writeRegistrationResponse(REG_DATA);
@@ -52,6 +74,14 @@ void setup(void)
  */
 void loop(void)
 {
+    int buttonState = digitalRead(BUTTON_PIN);
+    
+    if (buttonState != oldButtonState)
+    {
+      sendButtonState(buttonState);  
+      oldButtonState = buttonState;
+    }    
+    
     if (DH.read(rx_msg) == DH_PARSE_OK)
     {
         switch (rx_msg.intent)
@@ -61,16 +91,34 @@ void loop(void)
                 break;
 
 
-            case 256:
+            case 1000:
             {
-                const uint32_t cmd_id = rx_msg.getUInt32();
-                const uint8_t state = rx_msg.getUInt8();
+                const long cmd_id = rx_msg.getLong();
+                const byte state = rx_msg.getByte();
 
                 setLedState(state);
                 DH.writeCommandResult(cmd_id, CMD_STATUS_SUCCESS, CMD_RESULT_OK);
-
-                sendButtonState(state);
-            } break;
+                break;
+            } 
+            case 1001:
+            {
+              const long cmd_id = rx_msg.getLong();
+              
+              BlinkParam p;
+              
+              rx_msg.get(&p, sizeof(p));
+              
+              for (int i = 0; i < p.count; i++)
+              {
+                setLedState(1);
+                delay(p.on);
+                setLedState(0);
+                delay(p.off);  
+              }
+              
+              DH.writeCommandResult(cmd_id, CMD_STATUS_SUCCESS, CMD_RESULT_OK);
+              break;
+            }
         }
 
         rx_msg.reset(); // reset for the next message parsing
